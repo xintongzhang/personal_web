564f0c1ef92b862a8b86ab7c33996779190637d3
xintongzhang <annabelle.cmu@gmail.com>
2012-09-29 15:34:55 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 PersonalWeb::Application.routes.draw do
-get 'static_pages/home'
-get 'static_pages/about'
-get 'static_pages/contact'
-get 'static_pages/message'
-
- # match '/', to: 'static_pages#home'
-  #match '/about',   to: 'static_pages#about'
-  #match '/contact', to: 'static_pages#contact'
-  #match '/message', to: 'static_pages#message'
+  get "users/new"
+  get "users/new"
+  match '/signup',  to: 'users#new'
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+  root to: 'static_pages#home'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/message', to: 'static_pages#message'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,45 +3,38 @@ require 'spec_helper'
 describe "StaticPages" do
   
   describe "Home page" do
+    before { visit root_path } 
+    subject { page }
 
-     it "should have the h1 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home')
-    end
+    it {should have_selector('h1', :text => 'Home')}
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Xintong")
-    end
+
+    it {should have_selector('title',
+                        :text => "Xintong")}
+    
   end
 
   describe "About page" do
+    before { visit about_path } 
+    subject { page }
+    it {should have_selector('h1', :text => 'About')}
 
-    it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-    end
 
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "About")
-    end
+    it {should have_selector('title',
+                        :text => "About")}
+
   end
 
   describe "Contact page" do
+    before { visit contact_path } 
+    subject { page }
 
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
+    it {should have_selector('h1', :text => 'Contact')}
+    
 
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title',
-                    :text => "Contact")
-    end
+    it {should have_selector('title',
+                    :text => "Contact")}
+
   end
 
 
