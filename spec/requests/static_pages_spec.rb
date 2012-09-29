bac3bb8061dbd72d63224d81b3d91502527c7ed2
xintongzhang <annabelle.cmu@gmail.com>
2012-09-29 12:40:17 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+
+    it "should have the h1 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Home')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Home")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Contact")
+    end
+  end
+
+
+end
