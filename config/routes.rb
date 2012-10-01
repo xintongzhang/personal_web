bcdfa9467a9cfc12342e137a0671be2c0d1fcc85
xintongzhang <annabelle.cmu@gmail.com>
2012-10-01 16:19:07 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PersonalWeb::Application.routes.draw do
   resources :users
   root to: 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/signup',  to: 'users#new'
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
